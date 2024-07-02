6b4d0d3bbe58e67caf10d15887b1b0db84f48469
Kalvin Tang <ktang82@my.bcit.ca>
2024-07-01 17:11:44 -0700

    added footer and Javascript to get current year

---
+++ index.js
@@ -0,0 +1,6 @@
+let current_year = new Date().getFullYear;
+
+document.addEventListener("DOMContentLoaded", function () {
+    let current_year = new Date().getFullYear();
+    document.getElementById("year").textContent = current_year;
+});
