aad8be7ca45611b5b7bca87a2d4f606b567ee7e3
Kalvin Tang <ktang82@my.bcit.ca>
2024-07-09 20:53:18 -0700

    nav menu for mobile

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-let current_year = new Date().getFullYear;
+// Navbar toggle
 
+document.addEventListener("DOMContentLoaded", function () {
+    const navToggle = document.querySelector(".nav-toggle");
+    const navMenu = document.querySelector(".right-side-nav");
+
+    navToggle.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+        navToggle.textContent = navMenu.classList.contains("active")
+            ? "Close"
+            : "Menu";
+    });
+});
+
+// Footer year
 document.addEventListener("DOMContentLoaded", function () {
     let current_year = new Date().getFullYear();
     document.getElementById("year").textContent = current_year;
